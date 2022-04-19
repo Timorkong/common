1e137b06799520449a7c80a372f17ad57a3b566d
konglingzhao <450652415@qq.com>
2022-04-19 21:06:21 +0800

    init object

--- proto/compile.sh
+++ proto/compile.sh
@@ -5,5 +5,5 @@ for file in *.proto
 do
   arr=(${arr[*]} $file)
    #每一个proto文件执行一次
- protoc -I=./ --csharp_out=./../../lockStep/Assets/Code/Protobuf/Proto/ $file
+  protoc -I=./ --csharp_out=./../../lockStep/Assets/Code/Protobuf/Proto/ $file
 done
---
+++ proto/compileTs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#遍历所有的proto源文件
+source ~/.bash_profile
+for file in ./*.proto
+do
+  fullfile=$file
+  echo $fullfile
+  fullname=$(basename $fullfile)
+  dir=$(dirname $fullfile)
+  filename=$(echo $fullname | cut -d . -f1)
+  extension=$(echo $fullname | cut -d . -f2)
+  outputfile=$"./../compile/"$filename$".js"
+  tsfile=$"./../../LockstepServ/src/protobuff/"$filename$".d.ts"
+  echo $dir , $fullname , $filename , $extension , $outputfile
+   #每一个proto文件执行一次
+  pbjs -t static-module -w commonjs -o $outputfile $file
+  pbts -o $tsfile $outputfile
+done
