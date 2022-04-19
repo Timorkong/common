d7b4369fc3d0d849d14d09accb1f76d755e209fc
konglingzhao <450652415@qq.com>
2022-04-19 18:01:21 +0800

    init common

---
+++ proto/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#遍历所有的proto源文件
+source ~/.bash_profile
+for file in *.proto
+do
+  arr=(${arr[*]} $file)
+   #每一个proto文件执行一次
+ protoc -I=./ --csharp_out=./../../lockStep/Assets/Code/Protobuf/Proto/ $file
+done
