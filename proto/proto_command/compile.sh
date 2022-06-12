2df4db705a240cdf71a15fd0fba83c615ed89cc2
孔令召 <450652415@qq.com>
2022-06-12 19:50:21 +0800

    command

--- proto/proto_command/compile.sh
+++ proto/proto_command/compile.sh
@@ -7,10 +7,11 @@ do
   echo $fullfile
   fullname=$(basename $fullfile)
   filename=$(echo $fullname | cut -d . -f1)
-  outputfile_cs=$"./../../lockStep/Assets/Code/Protobuf/Proto/"$filename$".cs"
-  mono /usr/local/lib/node_modules/protogen/bin/protogen.exe -i:$file -o:$outputfile_cs
-  outputfile_js=$"./../../LockstepServ/src/protobuff/"$filename$".js"
-  outputfile_ts=$"./../../LockstepServ/src/protobuff/"$filename$".d.ts"
+  # outputfile_cs=$"./../../lockStep/Assets/Code/Protobuf/Proto/"$filename$".cs"
+  # mono /usr/local/lib/node_modules/protogen/bin/protogen.exe -i:$file -o:$outputfile_cs
+  outputfile_js=$"./../../../LockstepServ/src/protobuff/"$filename$".js"
+  outputfile_ts=$"./../../../LockstepServ/src/protobuff/"$filename$".d.ts"
+  echo $outputfile_js outputfile_ts
     #每一个proto文件执行一次
   pbjs -t static-module -w commonjs -o $outputfile_js $file
   pbts -o $outputfile_ts $outputfile_js
--- proto/proto_command/compileTs.sh
+++ proto/proto_command/compileTs.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 #遍历所有的proto源文件
-source ~/.bash_profile
+#source ~/.bash_profile
 for file in ./*.proto
 do
   fullfile=$file
   echo $fullfile
   fullname=$(basename $fullfile)
-  dir=$(dirname $fullfile)
-  filename=$(echo $fullname | cut -d . -f1)
-  extension=$(echo $fullname | cut -d . -f2)
-  outputfile_js=$"./../../LockstepServ/src/protobuff/"$filename$".js"
-  outputfile_ts=$"./../../LockstepServ/src/protobuff/"$filename$".d.ts"
-  echo $dir , $fullname , $filename , $extension , $outputfile
-   #每一个proto文件执行一次
+  # filename=$(echo $fullname | cut -d . -f1)
+  # outputfile_cs=$"./../../lockStep/Assets/Code/Protobuf/Proto/"$filename$".cs"
+  # mono /usr/local/lib/node_modules/protogen/bin/protogen.exe -i:$file -o:$outputfile_cs
+  outputfile_js=$"./../../../LockstepServ/src/protobuff/"$filename$".js"
+  outputfile_ts=$"./../../../LockstepServ/src/protobuff/"$filename$".d.ts"
+    #每一个proto文件执行一次
   pbjs -t static-module -w commonjs -o $outputfile_js $file
   pbts -o $outputfile_ts $outputfile_js
 done
